9303840a7906340fd70ae53735ede3def82108ef
Romihime <romi_de_munro@hotmail.com>
2025-11-19 20:03:21 -0300

    Fix restActionBuilderV3

--- core/src/main/kotlin/org/evomaster/core/problem/rest/builder/RestActionBuilderV3.kt
+++ core/src/main/kotlin/org/evomaster/core/problem/rest/builder/RestActionBuilderV3.kt
@@ -980,6 +980,8 @@ object RestActionBuilderV3 {
                         additionalFields = mutableListOf(),
                         attributeNames = attributeNames
                     )
+                }else{
+                    return createObjectGene(name, schema, schemaHolder,currentSchema, history, referenceClassDef, options, examples, messages)
                 }
             }
             //TODO file is a hack. I want to find a more elegant way of dealing with it (BMR)
